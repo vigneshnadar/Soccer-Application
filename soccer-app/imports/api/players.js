416cd54115bef6d1587f4baaddbbaa897793f7df
Vignesh Nadar <vigneshnadar@Vigneshs-MacBook-Pro.local>
2017-08-22 11:28:03 -0400

    added player form to be submitted

--- soccer-app/imports/api/players.js
+++ soccer-app/imports/api/players.js
@@ -2,18 +2,18 @@ import { Mongo } from 'meteor/mongo';
 
 export const Players = new Mongo.Collection('players');
 
-export PlayerSchema = new SimpleSchema({
-  name: { type : String },
-  team: { type : String },
-  ballManipuation: { type : Number, defaultValue:0},
-  kickingAbilities: { type : Number, defaultValue:0 },
-  passingAbilities: { type : Number, defaultValue:0 },
-  dualTackling: { type : Number, defaultValue:0 },
-  fieldCoverage: { type : Number, defaultValue:0 },
-  blockingAbilities: { type : Number, defaultValue:0 },
-  gameStrategy:{ type : Number , defaultValue:0},
-  playmakingRisks: { type : Number, defaultValue:0 },
-  notes: { type : String, optional:true },
+const PlayerSchema = new SimpleSchema({
+  name: {type : String },
+  team: {type : String },
+  ballManipuation: {type : Number, defaultValue:0},
+  kickingAbilities: {type : Number, defaultValue:0 },
+  passingAbilities: {type : Number, defaultValue:0 },
+  dualTackling: {type : Number, defaultValue:0 },
+  fieldCoverage: {type : Number, defaultValue:0 },
+  blockingAbilities: {type : Number, defaultValue:0 },
+  gameStrategy: {type : Number , defaultValue:0},
+  playmakingRisks: {type : Number, defaultValue:0 },
+  notes: {type : String, optional:true }
 });
 
 Players.attachSchema(PlayerSchema);
--- soccer-app/imports/ui/New.jsx
+++ soccer-app/imports/ui/New.jsx
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { browserHistory } from 'react-router';
+import { Players } from '../api/players';
 
 
 export default class New extends Component {
+
+  submitPlayer(event) {
+    //prevent from refreshing
+    event.preventDefault();
+
+    Players.insert({
+      name: this.refs.name.value,
+      team: this.refs.team.value,
+      ballManipuation: this.refs.ballManipuation.value,
+      kickingAbilities: this.refs.kickingAbilities.value,
+      passingAbilities: this.refs.passingAbilities.value,
+      dualTackling: this.refs.dualTackling.value,
+      fieldCoverage: this.refs.fieldCoverage.value,
+      blockingAbilities: this.refs.blockingAbilities.value,
+      gameStrategy: this.refs.gameStrategy.value,
+      playmakingRisks: this.refs.playmakingRisks.value,
+      notes: this.refs.notes.value,
+    });
+
+    console.log("Sucess player submitted!");
+
+    browserHistory.push('/');
+  }
+
   render() {
     return (
   <div className="row">
-    <form className="col s12">
+    <form className="col s12" onSubmit={this.submitPlayer.bind(this)}>
       <h3>Add a new player</h3>
 
       <div className="row">
         <div className="input-field col s6">
+
           <input placeholder="Name" ref="name" type="text" className="validate" />
         </div>
         <div className="input-field col s6">
@@ -18,15 +46,9 @@ export default class New extends Component {
       </div>
 
       <div className="row">
-        <div className="input-field col s6">
-          HELLOec
-          <select id="dropDown">
-  <option value="volvo" color="black">Volvo</option>
-  <option value="saab">Saab</option>
-  <option value="opel">Opel</option>
-  <option value="audi">Audi</option>
-</select>
-          <select ref="ballManipuation">
+        <div className="col s6">
+          <h5>Ball Manipulation</h5>
+          <select className="browser-default" ref="ballManipuation">
             <option value="0">0 - Hasn't demonstrated skills</option>
             <option value="1">1 - Needs improvement</option>
             <option value="2">2 - Skill acquired</option>
@@ -34,7 +56,8 @@ export default class New extends Component {
           </select>
         </div>
 
-        <div className="input-field col s6">
+        <div className="col s6">
+          <h5>Kicking Abilities</h5>
           <select className="browser-default" ref="kickingAbilities">
             <option value="0">0 - Hasn't demonstrated skills</option>
             <option value="1">1 - Needs improvement</option>
@@ -43,6 +66,76 @@ export default class New extends Component {
           </select>
         </div>
       </div>
+
+      <div className="row">
+        <div className="col s6">
+          <h5>Passing Abilities</h5>
+          <select className="browser-default" ref="passingAbilities">
+            <option value="0">0 - Hasn't demonstrated skills</option>
+            <option value="1">1 - Needs improvement</option>
+            <option value="2">2 - Skill acquired</option>
+            <option value="3">3 - Great skills/could teach</option>
+          </select>
+        </div>
+
+        <div className="col s6">
+          <h5>Dual Tackling</h5>
+          <select className="browser-default" ref="dualTackling">
+            <option value="0">0 - Hasn't demonstrated skills</option>
+            <option value="1">1 - Needs improvement</option>
+            <option value="2">2 - Skill acquired</option>
+            <option value="3">3 - Great skills/could teach</option>
+          </select>
+        </div>
+      </div>
+
+
+      <div className="row">
+        <div className="col s6">
+          <h5>Field Coverage</h5>
+          <select className="browser-default" ref="fieldCoverage">
+            <option value="0">0 - Hasn't demonstrated skills</option>
+            <option value="1">1 - Needs improvement</option>
+            <option value="2">2 - Skill acquired</option>
+            <option value="3">3 - Great skills/could teach</option>
+          </select>
+        </div>
+
+        <div className="col s6">
+          <h5>Blocking Abilities</h5>
+          <select className="browser-default" ref="blockingAbilities">
+            <option value="0">0 - Hasn't demonstrated skills</option>
+            <option value="1">1 - Needs improvement</option>
+            <option value="2">2 - Skill acquired</option>
+            <option value="3">3 - Great skills/could teach</option>
+          </select>
+        </div>
+      </div>
+
+
+
+            <div className="row">
+              <div className="col s6">
+                <h5>Game Strategy</h5>
+                <select className="browser-default" ref="gameStrategy">
+                  <option value="0">0 - Hasn't demonstrated skills</option>
+                  <option value="1">1 - Needs improvement</option>
+                  <option value="2">2 - Skill acquired</option>
+                  <option value="3">3 - Great skills/could teach</option>
+                </select>
+              </div>
+
+              <div className="col s6">
+                <h5>Play making risks</h5>
+                <select className="browser-default" ref="playmakingRisks">
+                  <option value="0">0 - Hasn't demonstrated skills</option>
+                  <option value="1">1 - Needs improvement</option>
+                  <option value="2">2 - Skill acquired</option>
+                  <option value="3">3 - Great skills/could teach</option>
+                </select>
+              </div>
+            </div>
+
 
       <div className="row">
         <div className="input-field col s6">
